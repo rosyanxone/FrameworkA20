982201b13a10c59862fce89bf63dfeaee7d28ef4
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-10-23 22:56:40 +0800

    test rm dir2

--- Posttest02/public/js/dark-mode.js
+++
@@ -1,7 +0,0 @@
-// penggunaan jquery
-$('#chk').change(() => {
-    $('.mode-bg').toggleClass('dark-bg'); 
-    $('.mode-text').toggleClass('light-text'); 
-    $('.mode-img').toggleClass('light-img');
-    $('.mode-border').toggleClass('light-border');
-});
